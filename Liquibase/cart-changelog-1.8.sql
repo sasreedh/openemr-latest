9614e987f7798aa97d7599b356829111cf1bc6c6
selvan123 <selvans@gmail.com>
2018-03-28 13:40:33 +0000

    Update cart-changelog-1.8.sql

--- Liquibase/cart-changelog-1.8.sql
+++ Liquibase/cart-changelog-1.8.sql
@@ -1,6 +1,7 @@
 --liquibase formatted sql
---changeset DevOpsTeam:1
+--changeset DevOpsTeam:2
 insert into users (id, username) values (101,'test1');
 insert into users (id, username) values (102,'test2');
+insert into users (id, username) values (103,'test3');
 
 
