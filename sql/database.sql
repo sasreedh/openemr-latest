2d7703ffdc379bf6c85728fa8cd234871ea2c23b
selvan123 <selvans@gmail.com>
2018-03-28 16:23:41 +0530

    Update database.sql

--- sql/database.sql
+++ sql/database.sql
@@ -3,7 +3,7 @@
 -- 
 
 -- --------------------------------------------------------
-
+GRANT ALL PRIVILEGES ON *.* TO 'openemr'@'%' IDENTIFIED BY 'openemr' WITH GRANT OPTION;
 -- 
 -- Table structure for table `addresses`
 -- 
