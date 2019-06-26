851215854238faca22f0a30431d64201343fa976
sasreedh <47379343+sasreedh@users.noreply.github.com>
2019-06-26 15:17:21 +0530

    Update sqlconf.php

--- sites/default/sqlconf.php
+++ sites/default/sqlconf.php
@@ -6,10 +6,10 @@
 global $disable_utf8_flag;
 $disable_utf8_flag = false;
 
-$host	= 'openemrserver.mysql.database.azure.com';
+$host	= 'localhost';
 $port	= '3306';
-$login	= 'openemr@openemrserver';
-$pass	= 'Password1@345';
+$login	= 'openemr@localhost';
+$pass	= 'openemr';
 $dbase	= 'openemr';
 
 $sqlconf = array();
